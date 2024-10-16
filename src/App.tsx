6ae5d590039cef961240781813af622e63a53e34
Rekha N Samudra <rekhansamudra@Rekha-N-Samudra.local>
2024-10-16 19:16:51 +0530

    update todo

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,40 @@ import ToDoList from './Components/ToDoList';
 const App: FC = () => {
   const [task, setTask] = useState("");
   const [todoList, setTodoList] = useState<ITask[]>([]);
+  const [isUpdating, setIsUpdating] = useState<boolean>(false); // Track if updating
+  const [updateIndex, setUpdateIndex] = useState<number | null>(null); // Index of task to update
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setTask(event.target.value)
   }
 
   const addTask = () => {
-    const newTask = { taskName: task }
-    setTodoList([...todoList, newTask])
-    setTask("")
-  }
+    if (isUpdating && updateIndex !== null) {
+      // If we are updating an existing task
+      const updatedList = todoList.map((todo, index) =>
+        index === updateIndex ? { ...todo, taskName: task } : todo
+      );
+      setTodoList(updatedList);
+      setIsUpdating(false); // Reset update mode
+      setUpdateIndex(null);  // Reset index
+    } else {
+      // Add new task
+      const newTask = { taskName: task };
+      setTodoList([...todoList, newTask]);
+    }
+    setTask(""); // Clear input field
+  };
 
   const deleteTask = (taskNameToDelete: string) => {
     setTodoList(todoList.filter((todo) => todo.taskName !== taskNameToDelete))
   }
+
+  const updateTask = (index: number) => {
+    const taskToUpdate = todoList[index].taskName;
+    setTask(taskToUpdate); 
+    setIsUpdating(true);   
+    setUpdateIndex(index); 
+  };
 
   return (
     <div className="App">
@@ -26,7 +46,7 @@ const App: FC = () => {
       <input type="text" placeholder='Enter Task...' onChange={handleInputChange} value={task} />
       <button onClick={addTask}>Add Task</button>
       {todoList.map((todo: ITask, index) => {
-        return <ToDoList key={index} todo={todo} deleteTask={deleteTask} />
+        return <ToDoList key={index} todo={todo} deleteTask={deleteTask} updateTask={() => updateTask(index)}/>
       })}
     </div>
   );
--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -3,14 +3,15 @@ import { ITask } from '../Interfaces'
 type ToDoListProps = {
     todo: ITask;
     deleteTask: (taskNameToDelete: string) => void
-
+    updateTask: () => void;
 }
 
-const ToDoList = ({ todo, deleteTask }: ToDoListProps) => {
+const ToDoList = ({ todo, deleteTask, updateTask }: ToDoListProps) => {
     return (
         <>
             <div>{todo.taskName}
                 <span><button onClick={() => deleteTask(todo.taskName)}>X</button></span>
+                <button onClick={updateTask}>Update</button> 
             </div>
         </>
     )
